51c459c4a570aaae416a002f2bcdee2560fa45c0
Kirill Klimov <ra22341@ya.ru>
2019-01-30 16:38:41 +0300

    empty case for evaluator

---
+++ tests/test_supervised_evaluator.py
@@ -0,0 +1,15 @@
+import unittest
+
+from collections import OrderedDict
+
+from .utils import is_ordered_dicts_almost_equal
+
+
+class TestSupervisedEvaluator(unittest.TestCase):
+    pass
+    # def test1(self):
+    #     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
