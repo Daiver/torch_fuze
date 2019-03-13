26791df999ff3c523a88a875a381b321a1336420
Kirill Klimov <ra22341@ya.ru>
2019-03-13 11:49:15 +0300

    add manual seed for every package which i use for dl

--- torch_fuze/utils/other.py
+++ torch_fuze/utils/other.py
@@ -1,7 +1,9 @@
 import copy
+import random
 from collections import OrderedDict
 import torch
 import torch.optim as optim
+import numpy as np
 
 
 def metrics_to_nice_string(metrics: OrderedDict):
@@ -15,3 +17,9 @@ def copy_model(model: torch.nn.Module) -> torch.nn.Module:
 def set_lr(optimizer: optim.Optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+
+def manual_seed(seed):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
